25f1528e1190ed5ebe0f3be070439be6532786fd
joggest95 <72763195+joggest95@users.noreply.github.com>
2024-04-11 22:40:27 -0400

    Update schemas.js

--- server/schemas.js
+++ server/schemas.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
 
-const User = mongoose.model('User', {
-    username: String,
-    email: String,
-    posts: Array,
-    createdAt: Date,
-    updatedAt: Date
-});
+const User = mongoose.model('User', new Schema({
+  username: String,
+  email: String,
+  posts: Array,
+  createdAt: Date,
+  updatedAt: Date
+  })
+);
 
 
 
 
 
-module.exports  = {User}
+module.exports  = {User};
