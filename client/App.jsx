c625b1e96d9e7cc0d6cc207f4d5a829486695649
Alex Thorne <alexandra.dr.thorne@gmail.com>
2024-04-13 15:15:49 -0700

    rest

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Login from './components/Authentication/Login.jsx';
+import Signup from './components/Authentication/Signup.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-    return (<div>This is a functional website</div>);
+    // will create routes to mainfeed and userprofile (maybe based on id or username)
+    return (
+    <Router>
+        <div>
+            <Routes>
+                <>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                </>
+            </Routes>
+        </div>
+    </Router>
+    );
 };
 
 export default App;
