3c7622dc8d7084bf367c76a26ace09ea1264a305
SameerSyed99 <ssyed1999@outlook.com>
2024-04-16 22:24:40 -0400

    integrating drawing app

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import { Provider } from 'react-redux';
-import App from "./App.jsx";
+import Canvas from "./Canvas.jsx";
 import style from "../style.css";
 import { createRoot } from 'react-dom/client';
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<Canvas/>);
---
+++ posts/uploads.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.listen(8080);
+
+app.get('./client/App.jsx', (req, res) => {
+    
+})
