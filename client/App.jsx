5daebf85ca7a8ce27b82270dfc6fee4a513480af
Alex Thorne <alexandra.dr.thorne@gmail.com>
2024-04-18 20:03:03 -0700

    App & Nav only while testing

--- client/App.jsx
+++ client/App.jsx
@@ -9,6 +9,8 @@ import { EditPostForm } from './features/posts/EditPostForm.jsx';
 import { Navbar } from './components/Navbar.jsx';
 import Cookies from 'js-cookie';
 import Canvas from './Canvas.jsx';
+import UsersList from './features/users/UsersList.jsx'
+import UserProfile from './features/users/UserProfile.jsx';
 
 function App() {
     // on app render, update userObj to null
@@ -17,12 +19,14 @@ function App() {
 
     useEffect(() => {
         let cookie = Cookies.get('isLoggedIn');
-        console.log(`cookie: ${JSON.stringify(cookie)}`);
+        // console.log(`cookie: ${JSON.stringify(cookie)}`);
+        // console.log("user obj ====> ", userObj);
+
         if(cookie === 'true'){
-            console.log('cookie true');
+            // console.log('cookie true');
             setCookiePresent(true);
         } else {
-            console.log('cookie false')
+            // console.log('cookie false')
             setCookiePresent(false);
         }
     })
@@ -30,22 +34,18 @@ function App() {
     useEffect(() => {
         async function fetchUser() {
             setUserObj(await fetch('/getUser'));
-            console.log("user obj ===> ", userObj);
         }
         if(cookiePresent){
             fetchUser();
+            // console.log("user obj ===> ", userObj);
+
         }
     },[cookiePresent]);
 
     return (
     <Router>
-<<<<<<< HEAD
-        <Navbar isLoggedIn={cookiePresent}/>
-=======
-        {/* <Canvas /> */}
-        <Navbar />
->>>>>>> origin
         <div className="App">
+            <Navbar isLoggedIn={cookiePresent}/>
             <Routes>
                 {cookiePresent ? (
                 <>
@@ -54,24 +54,32 @@ function App() {
                     element={
                         <React.Fragment>
                              <Canvas/>
-                            <PostsList />
+                            {/* <PostsList /> */}
                         </React.Fragment>} 
                 />
-                <Route
+                {/* <Route
                     path="/posts/:postId"
                     element={<SinglePostPage />}
                 />
                 <Route
                     path="/editPost/:postId"
                     element={<EditPostForm />}
-                /></>
+                /> */}
+                <Route
+                    path="/users"
+                    element={<UsersList />}
+                />
+                <Route
+                    path="/my-profile"
+                    element={<UserProfile />}
+            /></>
                     ) : (
                 <>
-                <Route 
+                {/* <Route 
                     path="/signup" 
                     element={
                         <Signup />
-                    } />
+                    } /> */}
                 <Route path="/" element={< Login />} />
                 </>)}
             </Routes>
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Navbar = (props) => {
-    // navLinks will want links to:
-    // main feed
-    // my profile
-    // log out button
-    console.log("Nav is logged in? ===> ", props.isLoggedIn);
+// export const Navbar = (props) => {
+//     // navLinks will want links to:
+//     // main feed
+//     // my profile
+//     // log out button
+
+
+export const Navbar = ({ isLoggedIn }) => {
+    // Function to handle logout
+    const handleLogout = () => {
+        // Send a request to log out the user
+        fetch('/auth/logout', {
+            method: 'GET',
+            credentials: 'include' // Include credentials to send cookies
+        })
+        .then(response => {
+            if (response.ok) {
+                // Redirect to the home page after logout
+                window.location.href = '/';
+            } else {
+                console.error('Failed to log out');
+            }
+        })
+        .catch(error => {
+            console.error('Error during logout:', error);
+        });
+    };
+    // console.log("Nav is logged in? ===> ", isLoggedIn);
     return (
         <nav>
             <section>
                 <h1>sktchr</h1>
                 <div className="navContent">
-                { props.isLoggedIn ? (
+                { isLoggedIn ? (
                     <div className="navLinks">
                         
-                        <Link to="/"> Posts </Link>
-                        <Link to="/logout">Log Out</Link>
+                        <Link to="/"> Home </Link>
+                        <Link to ='/my-profile'>My profile</Link>
+                        <Link to ='/users'>Users</Link>
+                        <button onClick={handleLogout}>Log Out</button>
                     </div>
                     ) : (<></>)}
                 </div>
